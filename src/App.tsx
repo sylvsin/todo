5722efc303f8ebacf2bd4c3ebf3e8839abd2fd78
sylvsin <lubwimi@gmail.com>
2020-06-28 17:46:49 +0200

    Added day and time

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ library.add(faTrash)
 
 interface IAppState {
   todoList: ITodo[]
+  time: string
 }
 
 class App extends React.Component<{}, IAppState> {
@@ -18,7 +19,8 @@ class App extends React.Component<{}, IAppState> {
     super(props)
 
     this.state = {
-      todoList: [{ todo:'Programmera', isDone:false}, { todo:'Gymmet', isDone:true}]
+      todoList: [{ todo:'Programmera', isDone:false}, { todo:'Gymmet', isDone:true}],
+      time: ''
     }
   }
 
@@ -43,15 +45,35 @@ class App extends React.Component<{}, IAppState> {
 
   deleteItem(todo: string) {
     const filteredItems = this.state.todoList.filter(item => item.todo!==todo);
-                          this.setState({
-                            todoList: filteredItems
-                          })
+    this.setState({
+      todoList: filteredItems
+    })
+  }
+
+  componentDidMount() {
+      setInterval(() => {
+        this.setState({
+          time: new Date().toLocaleString()
+      }) 
+    }, 1000);
   }
 
   render() {
+    const d = new Date();
+    const weekday = new Array(7);
+    weekday[0] = "Måndag";
+    weekday[1] = "Tisdag";
+    weekday[2] = "Onsdag";
+    weekday[3] = "Torsdag";
+    weekday[4] = "Fredag";
+    weekday[5] = "Lördag";
+    weekday[6] = "Söndag";
+
+    const n = weekday[d.getDay()];
+
     return(
       <div className="App">
-        <h2>Att Göra</h2>
+        <h4> Att göra idag {n + ' ' + this.state.time} </h4>
         <TodoList todos={this.state.todoList} addChangeTodo={this.changeTodoState.bind(this)} deleteItem={this.deleteItem.bind(this)}></TodoList>
         <NewTodo addTodo={this.updateTodoList.bind(this)}></NewTodo>
       </div>
